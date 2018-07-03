6c388779d9053203d4dd1ba3696475154fae767a
Fakhruddin Pata <fpata@apttus.com>
2018-07-03 17:11:18 +0530

    updated files to display data

--- DBGenInterface/Controllers/SchemaController.cs
+++ DBGenInterface/Controllers/SchemaController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Text;
 using DBGen;
 using DBGen.DBHelpers;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DBGenInterface.Controllers
 {
@@ -12,16 +14,16 @@ namespace DBGenInterface.Controllers
     public class SchemaController : Controller
     {
         // GET: api/Schema
-        [HttpGet]
-        public DataTable GetTables(string connectStr, DBType DbType)
+        [HttpPost]
+        public string GetTables(string connectStr, DBType DbType)
         {
             IDBHelper iDBHelper = DBFactory.GetDBInstance(connectStr, DbType);
             DataTable sqlTables = iDBHelper.GetTables();
-            return sqlTables;
+            return DataTableToJSON(sqlTables); 
         }
 
         // GET: api/Schema/5
-        [HttpGet]
+        [HttpPost]
         public DataTable GetColumns(string connectStr, DBType DbType, string tableName)
         {
             IDBHelper iDBHelper = DBFactory.GetDBInstance(connectStr, DbType);
@@ -30,7 +32,7 @@ namespace DBGenInterface.Controllers
         }
         
         // POST: api/Schema
-        [HttpGet]
+        [HttpPost]
         public String GetCode(string connectStr, DBType DbType,string tableName, string lang)
         {
             IDBHelper iDBHelper = DBFactory.GetDBInstance(connectStr, DbType);
@@ -38,6 +40,40 @@ namespace DBGenInterface.Controllers
             ICodeHelper codeHelper = DBFactory.GetCodeHelper(lang);
             String code = codeHelper.GetCode(tableName, sqlColumns, false);
             return code;
+        }
+
+        private string DataTableToJSON(DataTable table)
+        {
+            var JSONString = new StringBuilder();
+            if (table.Rows.Count > 0)
+            {
+                JSONString.Append("[");
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    JSONString.Append("{");
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        if (j < table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\",");
+                        }
+                        else if (j == table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\"");
+                        }
+                    }
+                    if (i == table.Rows.Count - 1)
+                    {
+                        JSONString.Append("}");
+                    }
+                    else
+                    {
+                        JSONString.Append("},");
+                    }
+                }
+                JSONString.Append("]");
+            }
+            return JSONString.ToString();
         }
     }
 }
---
+++ DBGenInterface/wwwroot/index.js
@@ -0,0 +1,59 @@
+﻿
+    function getAllTables(){
+        $.post("Schema/GetTables", {
+            "connectStr": $("#connectString")[0].value,
+            "DbType": $("input[name='DBType']:checked")[0].value
+            },
+            function (data, status) {
+                setTable(JSON.parse(data), document.getElementById("tableDetails"));
+                return data;
+    });    
+}
+
+function btnConnect_click() {
+    var stringData = getAllTables();
+    //var jsonData = JSON.parse(getAllTables);
+    
+    
+}
+
+function setTable(jsonData, divContainer) {
+
+    var col = [];
+    for (var i = 0; i < jsonData.length; i++) {
+        for (var key in jsonData[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+    table.classList.add("table");
+    table.classList.add("table-striped");
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < jsonData.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = jsonData[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
